38a99f714f4bbcdeb97f1b98937b14834633324c
Prashanth <prashanthkeshao.parate@scientificgames.com>
2021-11-25 12:43:51 +0530

    API handling using HTTP package

---
+++ jify_image_search/lib/service/api_helper.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:jify_image_search/model/image_model.dart';
+import 'package:http/http.dart' as http;
+import 'package:jify_image_search/utils/constants.dart';
+
+class ApiHelper {
+  Future<List<Hit>> getImages({String searchItem}) async {
+    final queryString = "&q=" + searchItem + "&image_type=photo&pretty=true";
+    var url = Constants.baseuri + Constants.apikey + queryString;
+    var response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      Map<String, dynamic> body = jsonDecode(response.body);
+      List<Hit> hits = body["hits"]
+          .map<Hit>(
+            (json) => Hit.fromJson(json),
+          )
+          .toList();
+      return hits;
+    } else {
+      throw Error();
+    }
+  }
+}
---
+++ jify_image_search/lib/utils/constants.dart
@@ -0,0 +1,9 @@
+/*
+API https://pixabay.com/api/?key=24493874-f9a7124a30de1deea37f19995&q=yellow+flowers&image_type=photo&pretty=true
+1.baseurl = https://pixabay.com/api/
+2.apikey = "24493874-f9a7124a30de1deea37f19995"
+*/
+class Constants {
+  static final baseuri = "https://pixabay.com/api/?";
+  static final apikey = "key=24493874-f9a7124a30de1deea37f19995";
+}
